98f8f286ea97ac872a33d69d1dbb1e52f16c4a43
Peter Lundgaard Thomsen <p_thom83@hotmail.com>
2017-09-03 11:26:31 +0200

    Changed sides so dark is first as it makes most sense to pick them first.

--- swapi/components/choose-side.js
+++ swapi/components/choose-side.js
@@ -24,15 +24,15 @@ export default class ChooseSide extends React.PureComponent {
 				<div className="row text-center">
 					<div className="side-menu">Choose your side.</div>
 					<div className="col-xs-6 side-menu-icon">
-						<span className={lightSide} onClick={() => chooseSide('light')}>
-							<i className="fa fa-rebel" />
-							<h3>The Light Side</h3>
-						</span>
-					</div>
-					<div className="col-xs-6 side-menu-icon">
 						<span className={darkSide} onClick={() => chooseSide('dark')}>
 							<i className="fa fa-empire" />
 							<h3>The Dark Side</h3>
+						</span>
+					</div>
+					<div className="col-xs-6 side-menu-icon">
+						<span className={lightSide} onClick={() => chooseSide('light')}>
+							<i className="fa fa-rebel" />
+							<h3>The Light Side</h3>
 						</span>
 					</div>
 				</div>
