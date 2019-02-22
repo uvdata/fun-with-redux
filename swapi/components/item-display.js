b69b799d76da471eeb46676b9976a27cc69c794c
Christian Fæster <chr.faester@gmail.com>
2019-02-22 01:03:35 +0100

    Adaps the list view depending on endpoint type. Refactors millenium-falcon.

--- swapi/components/item-display.js
+++ swapi/components/item-display.js
@@ -1,37 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class ItemDisplay extends React.PureComponent {
+const fontAwesomeIconMap = {
+	starships: 'space-shuttle',
+	people: 'user',
+	films: 'film'
+};
 
+class ItemDisplay extends React.PureComponent {
 	static propTypes = {
 		children: PropTypes.shape({
 			url: PropTypes.string.isRequired,
 			name: PropTypes.string.isRequired,
-			kind: PropTypes.string.isRequired,
+			kind: PropTypes.string.isRequired
 		})
 	};
 
 	state = {
-		isExpanded: false,
+		isExpanded: false
 	};
 
 	handleExpandToggle = () => {
-		this.setState((state) => ({ isExpanded: !state.isExpanded }));
+		this.setState(state => ({ isExpanded: !state.isExpanded }));
 	};
 
+	getFontAwesomeIcon(kind) {
+		return fontAwesomeIconMap[kind] || 'question-circle';
+	}
+
 	render() {
-		const { children: { name }, onExpandToggle} = this.props;
+		const { name, kind, onExpandToggle } = this.props;
 		const { isExpanded } = this.state;
 
-		return (<span>
-			<button className="btn btn-xs btn-default" onClick={this.handleExpandToggle} aria-label={isExpanded ? 'Collapse' : 'Expand'}>
-				{isExpanded
-					? <i className="fa fa-chevron-circle-up" />
-					: <i className="fa fa-chevron-circle-down" />}
-			</button>
-			{' '}
-			{name}
-			{ isExpanded ? <pre>{JSON.stringify(this.props.children, null, 4)}</pre> : null}
-		</span>);
+		return (
+			<span>
+				<button
+					className="btn btn-xs btn-default"
+					onClick={this.handleExpandToggle}
+					aria-label={isExpanded ? 'Collapse' : 'Expand'}
+				>
+					{isExpanded ? (
+						<i className="fa fa-chevron-circle-up" />
+					) : (
+						<i className="fa fa-chevron-circle-down" />
+					)}
+				</button>{' '}
+				<i className={'fa fa-' + this.getFontAwesomeIcon(kind)} />
+				{name}
+				{isExpanded ? (
+					<pre>{JSON.stringify(this.props.children, null, 4)}</pre>
+				) : null}
+			</span>
+		);
 	}
 }
+export default ItemDisplay;
--- swapi/components/millennium-falcon.js
+++ swapi/components/millennium-falcon.js
@@ -3,36 +3,70 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ItemDisplay from './item-display';
 
-export default class MillenniumFalcon extends React.PureComponent {
-
+class MillenniumFalcon extends React.PureComponent {
 	static propTypes = {
 		onChooseEndpoint: PropTypes.func.isRequired,
-		list: PropTypes.arrayOf(PropTypes.shape({
-			url: PropTypes.string.isRequired,
-		})).isRequired,
-		side: PropTypes.string,
+		list: PropTypes.arrayOf(
+			PropTypes.shape({
+				url: PropTypes.string.isRequired
+			})
+		).isRequired,
+		side: PropTypes.string
 	};
 
+	renderStarWarsResourceButton(endpoint, title, isLoading) {
+		return (
+			<button
+				disabled={isLoading}
+				onClick={() => this.props.onChooseEndpoint(endpoint)}
+				className="btn btn-danger"
+			>
+				{title}
+			</button>
+		);
+	}
+
+	renderItems(list) {
+		return (
+			<table className="table">
+				<tbody>
+					{list.map(item => (
+						<tr key={item.url}>
+							<td>
+								<ItemDisplay item={item} name={item.name} kind={item.kind} />
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
+
 	render() {
-		const { loading, list, onChooseEndpoint } = this.props;
+		const { loading, list } = this.props;
 
 		const iconClass = classNames('fa', {
 			'fa-refresh': loading,
 			'fa-spin': loading,
-			'fa-star': !loading,
+			'fa-star': !loading
 		});
 
-		return (<div>
-			<p><i className={iconClass} /> What do you want to see?</p>
-			<div className="btn-group">
-				<button disabled={loading} onClick={() => onChooseEndpoint('people')} className="btn btn-danger">People</button>
-				<button disabled={loading} onClick={() => onChooseEndpoint('films')} className="btn btn-danger">Films</button>
+		return (
+			<div>
+				<p>
+					<i className={iconClass} /> What do you want to see?
+				</p>
+				<div className="btn-group">
+					{this.renderStarWarsResourceButton('people', 'People', loading)}
+					{this.renderStarWarsResourceButton('films', 'Films', loading)}
+					{this.renderStarWarsResourceButton('starships', 'Starships', loading)}
+				</div>
+				<br />
+				<br />
+				{this.renderItems(list)}
 			</div>
-			<br /><br />
-			<table className="table"><tbody>
-					{list.map((item) => <tr key={item.url}><td><ItemDisplay>{item}</ItemDisplay></td></tr>)}
-				</tbody>
-			</table>
-		</div>);
+		);
 	}
 }
+
+export default MillenniumFalcon;
