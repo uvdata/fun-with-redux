bb3a87ed4032b3cc3a2aa08cb8aa4a2dce19ff2a
Christian Fæster <chr.faester@gmail.com>
2019-02-22 01:02:22 +0100

    Wired up Redux dev tools

--- swapi/store.js
+++ swapi/store.js
@@ -1,51 +1,58 @@
 import thunkMiddleware from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import * as types from './types';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const defaultData = {
 	endpoint: null,
 	data: {
 		/** will contain response from SWAPI, indexed by endpiont */
 	},
-	operations: 0,
+	operations: 0
 };
 
 const reducer = (state = {}, action) => {
-	switch(action.type) {
+	switch (action.type) {
 		case types.SET_DATA: {
 			const { endpoint, data } = action.payload;
 			return {
 				...state,
 				data: {
 					...state.data,
-					[endpoint]: data,
+					[endpoint]: data
 				}
-			}
+			};
 		}
 
 		case types.PICK_ENDPOINT: {
 			return {
 				...state,
-				endpoint: action.payload,
+				endpoint: action.payload
 			};
 		}
 
 		case types.START_LOAD: {
 			return {
 				...state,
-				operations: state.operations + 1,
-			}
+				operations: state.operations + 1
+			};
 		}
 
 		case types.DONE_LOAD: {
 			return {
 				...state,
-				operations: state.operations - 1,
-			}
+				operations: state.operations - 1
+			};
 		}
 	}
 
 	return state;
 };
 
-export default createStore(reducer, defaultData, applyMiddleware(thunkMiddleware));
+export default createStore(
+	reducer,
+	defaultData,
+	composeEnhancers(applyMiddleware(thunkMiddleware))
+);
